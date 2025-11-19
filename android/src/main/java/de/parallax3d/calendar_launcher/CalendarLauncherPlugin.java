8cab15a436889baa72797694c3b8cafab6a7ed1d
Jan Stuth <jan.stuth@audiosus.de>
2025-11-19 22:50:39 +0100

    fixed wrong implementation

--- android/src/main/java/de/parallax3d/calendar_launcher/CalendarLauncherPlugin.java
+++ android/src/main/java/de/parallax3d/calendar_launcher/CalendarLauncherPlugin.java
@@ -1,8 +1,12 @@
 package de.parallax3d.calendar_launcher;
 
+import android.annotation.TargetApi;
 import android.content.ComponentName;
 import android.content.Intent;
+import android.os.Build;
 import android.provider.CalendarContract;
+
+import androidx.annotation.NonNull;
 
 import java.text.DateFormat;
 import java.text.ParseException;
@@ -13,33 +17,66 @@ import java.util.Locale;
 import java.util.Map;
 import java.util.TimeZone;
 
+import io.flutter.embedding.engine.plugins.FlutterPlugin;
 import io.flutter.plugin.common.MethodCall;
 import io.flutter.plugin.common.MethodChannel;
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
 import io.flutter.plugin.common.MethodChannel.Result;
 
 /** CalendarLauncherPlugin */
-public class CalendarLauncherPlugin implements MethodCallHandler {
-
-    private static final String CHANNEL = "de.parallax3d/calendar_launcher";
+public class CalendarLauncherPlugin implements FlutterPlugin, MethodCallHandler {
+    /// The MethodChannel that will the communication between Flutter and native
+    /// Android
+    ///
+    /// This local reference serves to register the plugin with the Flutter Engine
+    /// and unregister it
+    /// when the Flutter Engine is detached from the Activity
+    private MethodChannel channel;
 
     @Override
-    public void configureFlutterEngine(@NonNull FlutterEngine flutterEngine) {
-        super.configureFlutterEngine(flutterEngine);
-        new MethodChannel(flutterEngine.getDartExecutor().getBinaryMessenger(), CHANNEL)
-            .setMethodCallHandler(
-            (call, result) -> {
-                if (call.method.equals("showSystemCalender")) {
-                    showSystemCalendar(call, result);
-                } else if (call.method.equals("requestCalendarAccess")) {
-                    requestCalendarAccess(result);
-                } else {
-                    result.notImplemented();
-                }
-            }
-        );
+    public void onAttachedToEngine(@NonNull FlutterPluginBinding flutterPluginBinding) {
+        channel = new MethodChannel(flutterPluginBinding.getBinaryMessenger(), "de.parallax3d/calendar_launcher");
+        channel.setMethodCallHandler(this);
     }
 
+    @Override
+    public void onMethodCall(@NonNull MethodCall call, @NonNull Result result) {
+        if (call.method.equals("showSystemCalender")) {
+            showSystemCalendar(call, result);
+        } else if (call.method.equals("requestCalendarAccess")) {
+            requestCalendarAccess(result);
+        } else {
+            result.notImplemented();
+        }
+    }
+
+    @Override
+    public void onDetachedFromEngine(@NonNull FlutterPluginBinding binding) {
+        channel.setMethodCallHandler(null);
+    }
+
+    // @Override
+    // public void configureFlutterEngine(@NonNull FlutterEngine flutterEngine) {
+    // super.configureFlutterEngine(flutterEngine);
+    // new MethodChannel(flutterEngine.getDartExecutor().getBinaryMessenger(),
+    // CHANNEL)
+    // .setMethodCallHandler(
+    // (call, result) -> {
+    // if (call.method.equals("showSystemCalender")) {
+    // if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.ICE_CREAM_SANDWICH) {
+    // showSystemCalendar(call, result);
+    // }
+    // } else if (call.method.equals("requestCalendarAccess")) {
+    // if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.ICE_CREAM_SANDWICH) {
+    // requestCalendarAccess(result);
+    // }
+    // } else {
+    // result.notImplemented();
+    // }
+    // });
+    // }
+
+    @androidx.annotation.RequiresApi(Build.VERSION_CODES.ICE_CREAM_SANDWICH)
     private void requestCalendarAccess(Result result) {
         Intent launchIntent = new Intent(Intent.ACTION_INSERT);
         launchIntent.setData(CalendarContract.Events.CONTENT_URI)
@@ -50,12 +87,10 @@ public class CalendarLauncherPlugin implements MethodCallHandler {
                 .putExtra(CalendarContract.Events.EVENT_LOCATION, "")
                 .putExtra(CalendarContract.Events.AVAILABILITY, CalendarContract.Events.AVAILABILITY_BUSY);
 
-        ComponentName componentName =
-                launchIntent.resolveActivity(mRegistrar.context().getPackageManager());
+        ComponentName componentName = launchIntent.resolveActivity(mRegistrar.context().getPackageManager());
 
-        boolean canLaunch =
-                componentName != null
-                        && !"{com.android.fallback/com.android.fallback.Fallback}"
+        boolean canLaunch = componentName != null
+                && !"{com.android.fallback/com.android.fallback.Fallback}"
                         .equals(componentName.toShortString());
         Map<String, Object> answer = new HashMap<>();
         answer.put("granted", canLaunch);
@@ -72,11 +107,13 @@ public class CalendarLauncherPlugin implements MethodCallHandler {
             if (date != null) {
                 return date.getTime();
             }
-        } catch (ParseException ignored) {}
+        } catch (ParseException ignored) {
+        }
 
         return System.currentTimeMillis();
     }
 
+    @androidx.annotation.RequiresApi(Build.VERSION_CODES.ICE_CREAM_SANDWICH)
     private void showSystemCalendar(MethodCall call, Result result) {
 
         String title = call.argument("title");
